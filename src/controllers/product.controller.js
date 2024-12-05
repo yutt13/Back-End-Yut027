df7d1d7887229ebc28d13d4db4463343a8b65c70
yutt13 <yutthanaa13@gmail.com>
2024-12-05 13:35:38 +0700

    first commit

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,12 @@
+// functions ที่ทำงานตาม route
+exports.get = (req, res) =>{
+   res.send("Get products here")
+}
+
+exports.getById =(req, res)=>{
+    res.send("Get products " + req.params.id);
+  };
+
+exports.create = (req, res)=>{
+    res.send("Create a product");
+  };
